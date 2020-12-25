8d73faeb3aeaca1831cc67b639a0498113abb857
mithrandyr <mithrandyr@users.noreply.github.com>
2020-12-24 23:56:23 -0500

    optimized solve for first puzzle

--- 2020/Day1a.ps1
+++ 2020/Day1a.ps1
@@ -1,5 +1,5 @@
 [cmdletBinding()]
-param([parameter(ValueFromPipeline)][int[]]$data)
+param([parameter(ValueFromPipeline)][int[]]$data, [ValidateSet("BruteForce","Intelligence")][string]$method = "Intelligence")
 
 <#
 Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
@@ -18,24 +18,50 @@ In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying the
 begin { $dataList = [int[]]@() }
 process{ $dataList += $data }
 end {
-    $searchCount = 0
-    $arrayLocation = 0    
-    $searchLocation = 1
-    while($true -or $arrayLocation -eq $dataList.Count) {
-        write-host "." -NoNewline
-        $searchCount += 1
-        #Write-Host ("Iteration #{0} --> {1}: {2} AND {3}: {4}" -f $searchCount, $arrayLocation, $dataList[$arrayLocation], $searchLocation, $dataList[$searchLocation])
-        if($dataList[$arrayLocation] + $dataList[$searchLocation] -eq 2020) {
-            Write-Host
-            Write-Verbose ("Took {0} iterations, solution found at {1}: {2} AND {3}: {4}" -f $searchCount, $arrayLocation, $dataList[$arrayLocation], $searchLocation, $dataList[$searchLocation])
-            return $dataList[$arrayLocation] * $dataList[$searchLocation]
+    <# -- Second Attempt: intelligence?
+        Was considering sorting the list and adding the biggest to smallest.. but then it hit me... I should only have to iterate through the list once...
+        calculating the required value each time, then search for that in the list.
+    #>
+    if($method -eq "Intelligence") {
+        $sw = [System.Diagnostics.Stopwatch]::StartNew()
+        $DiffList = @()
+        $searchCount = 0
+        foreach($d in $dataList) {
+            $searchCount += 1
+            $diff = 2020 - $d
+            if($DiffList.Contains($d)) {
+                Write-Host ("Time: {0}ms | Iterations: {1}" -f $sw.Elapsed.TotalMilliseconds, $searchCount)
+                $sw.stop()
+                $sw = $null
+                return $diff * $d
+            }
+            else { $DiffList += $diff }         
         }
-        else {
-            $searchLocation += 1
-            if($searchLocation -eq $dataList.Count) {
-                $arrayLocation += 1
-                $searchLocation = $arrayLocation
+    }
+
+    if($method -eq "BruteForce"){
+        $sw = [System.Diagnostics.Stopwatch]::StartNew()
+        $searchCount = 0
+        $arrayLocation = 0    
+        $searchLocation = 1
+        while($true -or $arrayLocation -eq $dataList.Count) {
+            #write-host "." -NoNewline
+            $searchCount += 1
+            #Write-Host ("Iteration #{0} --> {1}: {2} AND {3}: {4}" -f $searchCount, $arrayLocation, $dataList[$arrayLocation], $searchLocation, $dataList[$searchLocation])
+            if($dataList[$arrayLocation] + $dataList[$searchLocation] -eq 2020) {
+                Write-Host ("Time: {0}ms | Iterations: {1}" -f $sw.Elapsed.TotalMilliseconds, $searchCount)
+                $sw.stop()
+                $sw = $null
+                return $dataList[$arrayLocation] * $dataList[$searchLocation]
+            }
+            else {
+                $searchLocation += 1
+                if($searchLocation -eq $dataList.Count) {
+                    $arrayLocation += 1
+                    $searchLocation = $arrayLocation
+                }
             }
         }
     }
+
 }
